8346067fd374faf2e69bfc8f49b972328656ecc8
myusername <myemail>
2020-09-14 09:29:22 +0530

    final

--- src/app/_helpers/error.interceptor.ts
+++ src/app/_helpers/error.interceptor.ts
@@ -3,13 +3,13 @@ import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor} from '@angular/co
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
-import { Accountservice } from '@app/_services';
+import { AccountService } from '@app/_services';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor{
     constructor( private accountService: AccountService){}
 
-    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>{
+    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>>{
 
         return next.handle(request).pipe(catchError(err => {
             if([401,403].includes(err.status) && this.accountService.accountValue){
